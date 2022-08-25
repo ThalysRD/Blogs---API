0f173da97a753c04678837993054540e23bce846
ThalysRD <tgbr66@gmail.com>
2022-08-25 16:48:14 -0300

    Cria o model de users

---
+++ src/database/models/user.js
@@ -0,0 +1,21 @@
+
+const createUser = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    } ,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'Users',
+    timestamps: false,
+  });
+
+return User
+
+}
+
+module.exports = createUser
