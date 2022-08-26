793466f43c5dd4fb412562b7e6a3ad7793106304
ThalysRD <tgbr66@gmail.com>
2022-08-26 17:59:07 -0300

    Cria o middleware de validação de email

---
+++ src/middlewares/emailValidation.js
@@ -0,0 +1,18 @@
+const userService = require('../services/UserService');
+
+const verifyEmail = async (req, res, next) => {
+    const data = req.body;
+    if (!data.email) {
+        return res.status(400).json({ message: 'Some required fields are missing' });
+    }
+    const result = await userService.login(data);
+    if (result === null) {
+     return res.status(400).json({
+        message: 'Invalid fields',
+      });
+    }
+    
+    next();
+};
+
+module.exports = verifyEmail;
