ffc020a4a858b0eefd44f137275e6f53594998ca
ThalysRD <tgbr66@gmail.com>
2022-08-29 18:02:16 -0300

    Alteração do nome do arquivo

---
+++ src/services/userService.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../database/models');
+
+const secret = 'secretJWT';
+
+const login = async ({ email, password }) => {
+    const result = await User.findOne({ where: { email, password } });
+    if (result === null) {
+       return result;
+    }
+     const token = jwt.sign({ name: result.displayName },
+         secret, 
+         { algorithm: 'HS256', expiresIn: '1d' });
+    return token;
+};
+
+const registerUser = async ({ displayName, email, password, image }) => {
+    const ifExist = await User.findOne({ where: { email } });
+    if (ifExist === null) {
+     await User.create({ displayName, email, password, image });
+     const token = jwt.sign({ name: displayName }, secret, { algorithm: 'HS256', expiresIn: '1d' });
+       return token;
+    } 
+        return false;
+};
+
+const getAll = async () => {
+  const result = await User.findAll(); 
+  const newOjb = result.map(({ id, displayName, email, image }) => 
+  ({ id, displayName, email, image }));
+  return newOjb;
+};
+
+const getById = async ({ id }) => {
+ try {
+    const result = await User.findByPk(id);
+    delete result.dataValues.password;
+   return result;
+ } catch (e) {
+    return false;
+ }
+};
+
+module.exports = { login, registerUser, getAll, getById };
