15aab2c87b4f8133d1f7788f0f8ee5a00804dbea
ThalysRD <tgbr66@gmail.com>
2022-08-29 16:28:06 -0300

    Cria a rota para cadastrar o usuário

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const emailValidation = require('../middlewares/emailValidation');
 const passwordValidation = require('../middlewares/passwordValidation');
+const registerUser = require('../middlewares/registerUser');
 
 const usersRoute = express.Router();
 
@@ -9,5 +10,9 @@ usersRoute.post('/login',
 emailValidation, 
 passwordValidation, 
 userController.login);
+
+usersRoute.post('/user', 
+registerUser,
+ userController.registerUser);
 
 module.exports = usersRoute;
