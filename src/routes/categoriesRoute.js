ac0ecc2dad89ddfd3958f0ae72688a9cc6b0faaa
ThalysRD <tgbr66@gmail.com>
2022-08-30 16:20:12 -0300

    Adiciona middleware

--- src/routes/categoriesRoute.js
+++ src/routes/categoriesRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const categoriesController = require('../controllers/categoriesController');
+const jwtAuth = require('../middlewares/jwtAuth');
 
 const categoriesRoute = express.Router();
 
-categoriesRoute.post('/categories', categoriesController.createCategory);
-categoriesRoute.get('/categories', categoriesController.getAll);
+categoriesRoute.post('/categories', jwtAuth, categoriesController.createCategory);
+categoriesRoute.get('/categories', jwtAuth, categoriesController.getAll);
 
 module.exports = categoriesRoute;
