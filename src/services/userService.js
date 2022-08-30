c383ed40dd0c2ce835834eafea8cc4efd78eb9e4
ThalysRD <tgbr66@gmail.com>
2022-08-30 16:19:38 -0300

    Refatorando userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,16 +1,12 @@
-const jwt = require('jsonwebtoken');
+const { generateToken } = require('./jwtService');
 const { User } = require('../database/models');
-
-const secret = 'secretJWT';
 
 const login = async ({ email, password }) => {
     const result = await User.findOne({ where: { email, password } });
     if (result === null) {
        return result;
     }
-     const token = jwt.sign({ name: result.displayName },
-         secret, 
-         { algorithm: 'HS256', expiresIn: '1d' });
+     const token = generateToken(result.displayName);
     return token;
 };
 
@@ -18,7 +14,7 @@ const registerUser = async ({ displayName, email, password, image }) => {
     const ifExist = await User.findOne({ where: { email } });
     if (ifExist === null) {
      await User.create({ displayName, email, password, image });
-     const token = jwt.sign({ name: displayName }, secret, { algorithm: 'HS256', expiresIn: '1d' });
+     const token = generateToken(displayName);
        return token;
     } 
         return false;
