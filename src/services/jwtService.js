299ad7f4c2ac0aa57d4ad19280a75f822a7dfc3c
ThalysRD <tgbr66@gmail.com>
2022-08-30 18:38:17 -0300

    Refatorando o jwtService

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken');
 const secret = 'secretJWT';
 
 const jwtService = {
-    generateToken: (name) => {
-     const token = jwt.sign({ data: name }, secret, { expiresIn: '1d', algorithm: 'HS256' });
+    generateToken: (id) => {
+     const token = jwt.sign({ data: id }, secret, { expiresIn: '1d', algorithm: 'HS256' });
      return token;
     },
 
